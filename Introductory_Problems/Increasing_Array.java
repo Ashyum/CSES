9499b1c945849526a030aa78bef90bca86790881
Ashyum <ashyumplay@gmail.com>
2023-10-22 16:29:14 +0530

    update increasing_array

--- Introductory_Problems/Increasing_Array.java
+++ Introductory_Problems/Increasing_Array.java
@@ -12,14 +12,20 @@ public class Increasing_Array {
         int n=Integer.parseInt(br.readLine());
         String[] arr=new String[n];
         arr=br.readLine().split(" ");
-        long ans=0;
+        long ans=0,mx=0;
         for(int i=0;i<n;i++){
-            if(i-1>=0){
-                if(Integer.valueOf(arr[i-1])>Integer.valueOf(arr[i])) {
-                    ans+=(long)(Integer.valueOf(arr[i-1]) - Integer.valueOf(arr[i]));
-                    arr[i]=String.valueOf(Integer.valueOf(arr[i])+(Integer.valueOf(arr[i-1]) - Integer.valueOf(arr[i])));
-                }
-            }
+
+
+            mx=Math.max(mx,Integer.valueOf(arr[i]));
+            ans+=mx-Integer.valueOf(arr[i]);
+
+
+//            if(i-1>=0){
+//                if(Integer.valueOf(arr[i-1])>Integer.valueOf(arr[i])) {
+//                    ans+=(long)(Integer.valueOf(arr[i-1]) - Integer.valueOf(arr[i]));
+//                    arr[i]=String.valueOf(Integer.valueOf(arr[i])+(Integer.valueOf(arr[i-1]) - Integer.valueOf(arr[i])));
+//                }
+//            }
         }
         System.out.println(ans);
 
