7ab50e3a9a31a738c001417928667d084c7d22f6
Ashyum <ashyumplay@gmail.com>
2023-10-23 23:59:24 +0530

    updated two knights

--- Introductory_Problems/Two_Knights.java
+++ Introductory_Problems/Two_Knights.java
@@ -8,6 +8,13 @@ public class Two_Knights {
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
+        int n=Integer.parseInt(br.readLine());
 
+        long total_comb=0,attack=0;
+        for(int i=1;i<=n;i++){
+            total_comb=((i*i)*((i*i)-1))/2;
+            attack=4*(i-1)*(i-2);
+            System.out.println(total_comb-attack);
+        }
     }
 }
