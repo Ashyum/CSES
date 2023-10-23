2151cc7bd5fe4fa82f6728a5f77f409b29e9846d
Ashyum <ashyumplay@gmail.com>
2023-10-23 23:53:00 +0530

    two knights

---
+++ Introductory_Problems/Two_Knights.java
@@ -0,0 +1,13 @@
+package Introductory_Problems;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Two_Knights {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
