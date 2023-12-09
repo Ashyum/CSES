51946d79765902245581f0d51db2e25fcd689e29
Ashyum <ashyumplay@gmail.com>
2023-12-10 00:00:14 +0530

    ADDED DP SOLUTIONS

---
+++ Dynamic_Programming/Dice_Combinations.java
@@ -0,0 +1,19 @@
+//package Dynamic_Programming;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        System.out.println(helper(n));
+    }
+    private static int helper(int n){
+        int ans=0;
+        for(int i=1;i<7;i++){
+
+        }
+    }
+}
